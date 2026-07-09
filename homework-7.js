5100a6863582e8a67bb3d4feb81cd54406495249
aiganym <Aiganymgalym92@gmail.com>
2026-07-09 20:47:15 +0500

    (homework №7): basic entities in JS

---
+++ homework-7.js
@@ -0,0 +1,32 @@
+function showWeather(city,temperature) { 
+  console.log(`сейчас в городе ${city} температура ${temperature} градусов`);
+}
+
+showWeather('Москва', 20);
+
+const light_speed = '299 792 458';
+
+function showSpeed(speed) {
+  if (speed > light_speed) {
+    console.log('Сверхсветовая скорость');
+  } else if (speed === light_speed) {
+    console.log('Световая скорость');
+  } else {
+    console.log('субсветовая скорость');
+  }
+}
+
+showSpeed('299 792 457');
+
+let variableNumber1 = 'milk';
+let variableNumber2 = 'price';
+
+function milkPrice(currentBudget, variableNumber2) {
+  if (currentBudget > variableNumber2) {
+    console.log(` ${variableNumber1} товар приобретен. спасибо за покупку`);
+  } else {
+    console.log(` вам не хватает ${variableNumber2 - currentBudget} пополните баланс`);
+  }
+}
+
+milkPrice(100, 150);
