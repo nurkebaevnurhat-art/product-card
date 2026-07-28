94fd0f0f06403981d5c35295a7de1852f89cfde8
aiganym <Aiganymgalym92@gmail.com>
2026-07-28 08:48:18 +0500

    (homework №8): исправил ошибки по дз

--- homework-8.js
+++ homework-8.js
@@ -43,95 +43,95 @@ console.log(products);
 
 // № 8 массив с книгами
 const books = [
-{
-  title: "1984",
-  author: "Джордж Оруэлл",
-  year: 1949,
-  coverColor: "серый",
-  genre: "антиутопия"
+  {
+    title: "1984",
+    author: "Джордж Оруэлл",
+    year: 1949,
+    coverColor: "серый",
+    genre: "антиутопия"
   },
-{
-  title: "Мастер и Маргарита",
-  author: "Михаил Булгаков",
-  year: 1967,
-  coverColor: "синий",
-  genre: "роман"
-},
   {
-  title: "Граф Монте-Кристо",
-  author: "Александр Дюма",
-  year: 1844,
-  coverColor: "коричневый",
-  genre: "приключения"
-},
+    title: "Мастер и Маргарита",
+    author: "Михаил Булгаков",
+    year: 1967,
+    coverColor: "синий",
+    genre: "роман"
+  },
   {
-  title: "Автостопом по галактике",
-  author: "Дуглас Адамс",
-  year: 1979,
-  coverColor: "зелёный",
-  genre: "научная фантастика"
-}
+    title: "Граф Монте-Кристо",
+    author: "Александр Дюма",
+    year: 1844,
+    coverColor: "коричневый",
+    genre: "приключения"
+  },
+  {
+    title: "Автостопом по галактике",
+    author: "Дуглас Адамс",
+    year: 1979,
+    coverColor: "зелёный",
+    genre: "научная фантастика"
+  }
 ];
 
 books.push({
-  title: "Дюна",
-  author: "Фрэнк Герберт",
-  year: 1965,
-  coverColor: "оранжевый",
-  genre: "научная фантастика"
+    title: "Дюна",
+    author: "Фрэнк Герберт",
+    year: 1965,
+    coverColor: "оранжевый",
+    genre: "научная фантастика"
 });
 
 console.log(books);
 
 // № 9 объединение массивов
 const starWarsBooks = [
-{
-  title: "Звёздные войны. Эпизод IV: Новая надежда",
-  author: "Алан Дин Фостер (под псевдонимом Джордж Лукас)",
-  year: 1976,
-  isRare: true,
-},
-{
-  title: "Траун",
-  author: "Тимоти Заан",
-  year: 2017,
-  isRare: false,
-},
-{
-  title: "Наследник Империи",
-  author: "Тимоти Заан",
-  year: 1991,
-  isRare: true,
-},
-{
-  title: "Дарт Плэгас",
-  author: "Джеймс Лусено",
-  year: 2012,
-  isRare: false,
-},
-{
-  title: "Свет джедаев",
-  author: "Чарльз Соул",
-  year: 2021,
-  isRare: false,
-},
-{
-  title: "Утерянные звёзды",
-  author: "Клаудия Грей",
-  year: 2015,
-  isRare: false,
-}
+  {
+    title: "Звёздные войны. Эпизод IV: Новая надежда",
+    author: "Алан Дин Фостер (под псевдонимом Джордж Лукас)",
+    year: 1976,
+    isRare: true,
+  },
+  {
+    title: "Траун",
+    author: "Тимоти Заан",
+    year: 2017,
+    isRare: false,
+  },
+  {
+    title: "Наследник Империи",
+    author: "Тимоти Заан",
+    year: 1991,
+    isRare: true,
+  },
+  {
+    title: "Дарт Плэгас",
+    author: "Джеймс Лусено",
+    year: 2012,
+    isRare: false,
+  },
+  {
+    title: "Свет джедаев",
+    author: "Чарльз Соул",
+    year: 2021,
+    isRare: false,
+  },
+  {
+    title: "Утерянные звёзды",
+    author: "Клаудия Грей",
+    year: 2015,
+    isRare: false,
+  }
 ];
   const allBooks = [...books, ...starWarsBooks];
 
 // № 10 добавление свойства old в объект книги
 const booksWithOldProperty = allBooks.map(book => {
-if (book.year < 2000) {
-  book.old = true;
+  if (book.year < 2000) {
+    book.old = true;
 }
-else {
-  book.old = false;
+  else {
+    book.old = false;
 }
-return book;
+  return book;
 });
   console.log(booksWithOldProperty);
