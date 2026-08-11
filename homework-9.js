57106e9e1203069db2a5c1242445315a03f83fe3
aiganym <Aiganymgalym92@gmail.com>
2026-08-11 22:26:42 +0500

    (homework №9): исправил ошибки по дз

--- homework-9.js
+++ homework-9.js
@@ -2,7 +2,7 @@ import { usersComments } from './comments.js';
 
 // задание №2
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
-const newNumbers= numbers.filter(number => number>=5);
+const newNumbers = numbers.filter(number => number >= 5);
 
 console.log(newNumbers);
 
@@ -13,7 +13,7 @@ const isPresent = furniture.includes('sofa');
 console.log(isPresent);
 
 // задание №4
-function reverseArray(arr) {
+function reverseArray (arr) {
 return arr.reverse();
 }
 
@@ -21,7 +21,7 @@ console.log(reverseArray(numbers));
 console.log(reverseArray(furniture));
 
 // задание №7
-const mailComUsers= usersComments.filter(user => user.email.includes ('.com'));
+const mailComUsers = usersComments.filter(user => user.email.includes ('.com'));
 
 console.log(mailComUsers);
 
@@ -36,10 +36,10 @@ const dataUsers = usersComments.map(({id, name}) => ({id, name}));
 console.log(dataUsers);
 
 // задание №10
-const updatedUsersComments={...usersComments, isInvalid: 180};
+const updatedUsersComments = {...usersComments, isInvalid: 180};
 
 console.log(updatedUsersComments);
 
-const addPropertyUserComments = usersComments.map(user => ({...user, isInvalid: user.body.length < 180 ? true: false}));
+const PropertyUserComments = usersComments.map(user => ({...user, isInvalid: user.body.length < 180 ? true: false}));
 
-console.log(addPropertyUserComments);
+console.log(PropertyUserComments);
